a95720db723b5f5f1aea7d0e75b69ef910f38502
Olivier Bossel <olivier.bossel@gmail.com>
2019-01-23 18:29:46 +0100

    add s-router back

--- src/js/router.js
+++ src/js/router.js
@@ -2,7 +2,7 @@ import SRouterComponent from 'coffeekraken-s-router-component'
 import { contactHandler, contactLeave } from './pages/contact'
 import { projectsHandler } from './pages/projects'
 
-setTimeout(() => {
+document.body.addEventListener('app:ready', () => {
   SRouterComponent.on('/', projectsHandler)
     .on('/contact', contactHandler, {
       leave: contactLeave

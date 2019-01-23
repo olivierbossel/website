6c4fe266e6c0143034af842f39efaec14567c320
Olivier Bossel <olivier.bossel@gmail.com>
2019-01-23 18:11:22 +0100

    Revert "Revert "integrate the s-router component in place of navigo""
    This reverts commit 3dad9e14c546970640b59b828382b1dca4afb3ec.

--- src/js/router.js
+++ src/js/router.js
@@ -1,26 +1,11 @@
-import Navigo from 'navigo'
-import { contactEnterHandler, contactLeaveHandler } from './pages/contact'
-import { projectsEnterHandler } from './pages/projects'
+import SRouterComponent from 'coffeekraken-s-router-component'
+import { contactHandler, contactLeave } from './pages/contact'
+import { projectsHandler } from './pages/projects'
 
-const root = '/'
-const useHash = false
-const hash = '#'
-const router = new Navigo(root, useHash, hash)
-
-router.hooks({
-  before: done => {
-    if (window.isIntroRunning) {
-      document.body.addEventListener('app:ready', () => {
-        done()
-      })
-      return
-    }
-    done()
-  }
+setTimeout(() => {
+  SRouterComponent.on('/', projectsHandler)
+    .on('/contact', contactHandler, {
+      leave: contactLeave
+    })
+    .listen()
 })
-router
-  .on('contact', contactEnterHandler, {
-    leave: contactLeaveHandler
-  })
-  .on('', projectsEnterHandler)
-  .resolve()
